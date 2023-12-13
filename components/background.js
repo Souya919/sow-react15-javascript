e95765e83c1fc139a905d94aac5d30aedb65d1ec
Souya919 <souyahanda3@gmail.com>
2023-12-13 21:45:06 +0800

    fix white space issue

--- components/background.js
+++ components/background.js
@@ -1,15 +1,7 @@
 var Background = React.createClass({
   render: function () {
-    return React.createElement(
-      "div",
-      {
-        className: "background",
-      },
-      React.createElement("img", {
-        id: "background-img",
-        src: "https://storage.123fakturere.no/public/wallpapers/geiranger.jpg",
-        alt: "Image",
-      })
-    );
+    return React.createElement("div", {
+      className: "background",
+    });
   },
 });
